a1967c66e933d31d697ebec6f15c2f8e13fcd3d2
moparisthebest <admin@moparisthebest.com>
2022-10-01 00:08:42 -0400

    Add basic systray support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,9 @@
 )]
 
 use std::sync::Arc;
+use tauri::{
+    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
 use tokio::net::TcpListener;
 use xmpp_proxy::{
     common::{certs_key::CertsKey, outgoing::OutgoingConfig},
@@ -28,7 +31,56 @@ async fn start_proxy() -> u16 {
 }
 
 fn main() {
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new("show".to_string(), "Show"))
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("hide".to_string(), "Hide"))
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("quit".to_string(), "Quit"));
+    let system_tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a left click");
+                // doesn't seem to work
+            }
+            SystemTrayEvent::RightClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a right click");
+                // doesn't seem to work
+            }
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a double click");
+                // doesn't seem to work
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                "show" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![start_proxy])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
